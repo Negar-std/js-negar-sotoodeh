0166bcc7772699654e879b128664bc6bfd5863b2
Negar Sotoodeh <Negar.stdh@gmail.com>
2023-07-29 09:56:21 +0330

    implement js-04-3 exercise

---
+++ js-04/js-04-3/index.js
@@ -0,0 +1,16 @@
+const product = {
+    name: "power bank" ,
+    brand: "Anker" ,
+    color: "black" ,
+    price: 1000000 ,
+    warranty: "fars" ,
+    seller: "digikala" ,
+
+    add: function () {
+        console.log("Product added to cart.")
+    }
+    ,
+    remove: function () {
+        console.log("Product removed from cart.")
+    }
+}
